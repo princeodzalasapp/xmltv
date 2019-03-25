195bd8dcf8ab916e1c6385f2c12c2ae8433e2f37
Sylvain CECCHETTO <cecchetto.sylvain@me.com>
2019-03-25 20:33:02 +0100

    Update script

--- update_all_tv_guides.sh
+++ update_all_tv_guides.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+now2=`date`
+echo -e "- Start script at $now2"
+
 BASEDIR=$(dirname "$0")
 
 echo -e "- Script directory: $BASEDIR --> Let's move to this directory"
@@ -16,10 +19,10 @@ git reset --hard origin/master
 
 
 echo -e "\n# Update tv_guide_fr.xml\n"
-./fr/tv_grab_fr --config-file ./fr/tv_grab_fr_config.txt --days 7 --ch_prefix "" --ch_postfix "" --output ./tv_guide_fr.xml
+./fr/tv_grab_fr --config-file ./fr/tv_grab_fr_config.txt --days 7 --output ./tv_guide_fr.xml
 
 echo -e "\n# Update tv_guide_be.xml\n"
-./be/tv_grab_be --config-file ./be/tv_grab_be_config.txt --days 7 --ch_prefix "" --ch_postfix "" --output ./tv_guide_be.xml
+./be/tv_grab_be --config-file ./be/tv_grab_be_config.txt --days 7 --output ./tv_guide_be.xml
 
 
 echo -e "\n# Update tv_guide_all.xml\n"
@@ -28,6 +31,7 @@ tv_sort --by-channel --output tv_guide_be_sorted.xml tv_guide_be.xml
 tv_merge -i tv_guide_fr_sorted.xml -m tv_guide_be_sorted.xml -o tv_guide_all.xml
 rm tv_guide_fr_sorted.xml
 rm tv_guide_be_sorted.xml
+
 
 now=$(date +"%d/%m/%Y")
 
