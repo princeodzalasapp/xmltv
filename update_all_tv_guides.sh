60b5d8bbc8ea64b4dc5e1b913bc5cbdd11872c1b
Sylvain CECCHETTO <cecchetto.sylvain@me.com>
2019-11-15 17:23:14 +0100

    Add tv guide lite

--- update_all_tv_guides.sh
+++ update_all_tv_guides.sh
@@ -1,43 +1,48 @@
 #!/usr/bin/env bash
 
-now=$(date +"%d/%m/%Y")
-echo -e "- Start script at $now"
-
 BASEDIR=$(dirname "$0")
-
-echo -e "- Script directory: $BASEDIR --> Let's move to this directory"
-
 cd $BASEDIR
 cwd=$(pwd)
+LOGFILE=$cwd/log.txt
 
-echo -e ""
-echo -e "- Current workning directory: $cwd"
+now=$(date +"%d/%m/%Y")
+echo -e "- Start script at $now\n" > ${LOGFILE}
 
-echo -e "- To avoid any git conflict we do a force pull first\n"
-git fetch --all
-git reset --hard origin/master
+echo -e "- To avoid any git conflict we do a force pull first\n" >> ${LOGFILE}
+git fetch --all 2>&1 >> ${LOGFILE}
+git reset --hard origin/master 2>&1 >> ${LOGFILE}
 
+echo -e "\n# Update tv_guide_fr.xml\n" >> ${LOGFILE}
+rm tv_guide_fr.xml
+./fr/tv_grab_fr --config-file ./fr/tv_grab_fr_config.txt --days 7 --offset -1 --output ./tv_guide_fr.xml 2>&1 >> ${LOGFILE}
 
-echo -e "\n# Update tv_guide_fr.xml\n"
-./fr/tv_grab_fr --config-file ./fr/tv_grab_fr_config.txt --days 7 --output ./tv_guide_fr.xml
+echo -e "\n# Update tv_guide_be.xml\n" >> ${LOGFILE}
+rm tv_guide_be.xml
+./be/tv_grab_be --config-file ./be/tv_grab_be_config.txt --days 7 --offset -1 --output ./tv_guide_be.xml 2>&1 >> ${LOGFILE}
 
-echo -e "\n# Update tv_guide_be.xml\n"
-./be/tv_grab_be --config-file ./be/tv_grab_be_config.txt --days 7 --output ./tv_guide_be.xml
-
-
-echo -e "\n# Update tv_guide_all.xml\n"
-tv_sort --by-channel --output tv_guide_fr_sorted.xml tv_guide_fr.xml
-tv_sort --by-channel --output tv_guide_be_sorted.xml tv_guide_be.xml
-tv_merge -i tv_guide_fr_sorted.xml -m tv_guide_be_sorted.xml -o tv_guide_all.xml
+echo -e "\n# Update tv_guide_all.xml\n" >> ${LOGFILE}
+tv_sort --by-channel --output tv_guide_fr_sorted.xml tv_guide_fr.xml 2>&1 >> ${LOGFILE}
+tv_sort --by-channel --output tv_guide_be_sorted.xml tv_guide_be.xml 2>&1 >> ${LOGFILE}
+rm tv_guide_all.xml
+tv_merge -i tv_guide_fr_sorted.xml -m tv_guide_be_sorted.xml -o tv_guide_all.xml 2>&1 >> ${LOGFILE}
 rm tv_guide_fr_sorted.xml
 rm tv_guide_be_sorted.xml
 
+echo -e "\n# Update tv_guide_fr_lite.zip\n" >> ${LOGFILE}
+rm tv_guide_fr_lite.zip
+./fr/tv_grab_fr --config-file ./fr/tv_grab_fr_config.txt --days 2 --offset -1 --output ./tv_guide_fr_lite.xml 2>&1 >> ${LOGFILE}
+zip tv_guide_fr_lite.zip tv_guide_fr_lite.xml
+rm tv_guide_fr_lite.xml
 
+echo -e "\n# Update tv_guide_be_lite.zip\n" >> ${LOGFILE}
+rm tv_guide_be_lite.zip
+./be/tv_grab_be --config-file ./be/tv_grab_be_config.txt --days 2  --offset -1 --output ./tv_guide_be_lite.xml 2>&1 >> ${LOGFILE}
+zip tv_guide_be_lite.zip tv_guide_be_lite.xml
+rm tv_guide_be_lite.xml
 
-git add --all
-git commit -m "Auto update TV guides ($now)"
-git push
-echo -e "\t* Changes have been pushed"
-
+git add --all 2>&1 >> ${LOGFILE}
+git commit -m "Auto update TV guides ($now)" 2>&1 >> ${LOGFILE}
+git push 2>&1 >> ${LOGFILE}
+echo -e "\t* Changes have been pushed" >> ${LOGFILE}
 
 exit 0
