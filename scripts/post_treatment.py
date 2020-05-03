cba60bb210a14ade733a325ebbf6b4a128a2df80
Sylvain CECCHETTO <cecchetto.sylvain@me.com>
2020-05-03 18:43:31 +0200

    [fix] Missing France 5 in TNT files

--- scripts/post_treatment.py
+++ scripts/post_treatment.py
@@ -44,34 +44,33 @@ countries = {
         'dst_fp': os.path.join(WD, '../tv_guide_fr_tnt{}.xml'),
         'tz': 'Europe/Paris',
         'channels_to_add': [
-            'C192.api.telerama.fr',
-            'C4.api.telerama.fr',
-            'C80.api.telerama.fr',
-            'C34.api.telerama.fr',
-            'C45.api.telerama.fr',
-            'C118.api.telerama.fr',
-            'C111.api.telerama.fr',
-            'C4.api.telerama.fr',
-            'C445.api.telerama.fr',
-            'C119.api.telerama.fr',
-            'C195.api.telerama.fr',
-            'C1404.api.telerama.fr',
-            'C444.api.telerama.fr',
-            'C446.api.telerama.fr',
-            'C234.api.telerama.fr',
-            'C78.api.telerama.fr',
-            'C481.api.telerama.fr',
-            'C226.api.telerama.fr',
-            'C458.api.telerama.fr',
-            'C482.api.telerama.fr',
-            'C160.api.telerama.fr',
-            'C1401.api.telerama.fr',
-            'C1403.api.telerama.fr',
-            'C1402.api.telerama.fr',
-            'C1400.api.telerama.fr',
-            'C1399.api.telerama.fr',
-            'C112.api.telerama.fr',
-            'C2111.api.telerama.fr'
+            'C192.api.telerama.fr', # TF1
+            'C4.api.telerama.fr', # France 2
+            'C80.api.telerama.fr', # France 3
+            'C34.api.telerama.fr', # Canal+
+            'C47.api.telerama.fr', # France 5
+            'C118.api.telerama.fr', # M6
+            'C111.api.telerama.fr', # Arte
+            'C445.api.telerama.fr', # C8
+            'C119.api.telerama.fr', # W9
+            'C195.api.telerama.fr', # TMC
+            'C446.api.telerama.fr', # TFX
+            'C444.api.telerama.fr', # NRJ 12
+            'C234.api.telerama.fr', # LCP
+            'C78.api.telerama.fr', # France 4
+            'C481.api.telerama.fr', # BFM TV
+            'C226.api.telerama.fr', # CNEWS
+            'C458.api.telerama.fr', # CSTAR
+            'C482.api.telerama.fr', # Gulli
+            'C160.api.telerama.fr', # France O
+            'C1404.api.telerama.fr', # TF1 Séries Films
+            'C1401.api.telerama.fr', # L'équipe
+            'C1403.api.telerama.fr', # 6ter
+            'C1402.api.telerama.fr', # RMC Story
+            'C1400.api.telerama.fr', # RMC Découverte
+            'C1399.api.telerama.fr', # Chérie 25
+            'C112.api.telerama.fr', # LCI
+            'C2111.api.telerama.fr' # Franceinfo
         ]
     },
     'be': {
